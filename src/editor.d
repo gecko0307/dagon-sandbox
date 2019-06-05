8f2e3945de771c91911298f8b5aaa9075a4dc210
TimurGafarov <gt@smarthead.ru>
2019-06-05 13:13:47 +0300

    Updated dagon submodule

--- src/editor.d
+++ src/editor.d
@@ -33,13 +33,13 @@ import dagon;
 class AttachToCamera: EntityComponent
 {
     Camera camera;
-    
+
     this(EventManager em, Entity e, Camera camera)
     {
         super(em, e);
         this.camera = camera;
     }
-    
+
     override void update(Time time)
     {
         entity.position = camera.positionAbsolute;
@@ -59,76 +59,76 @@ class AttachToCamera: EntityComponent
 class Editor: Scene
 {
     Game game;
-    
+
     LoadingScreen loadingScreen;
-    
+
     FontAsset aFont;
-    
+
     OBJAsset aMeshCerberus;
     TextureAsset aTexCerberusAlbedo;
     TextureAsset aTexCerberusNormal;
     TextureAsset aTexCerberusRoughness;
     TextureAsset aTexCerberusMetallic;
-    
+
     ImageAsset aHeightmap;
     TextureAsset aTexDesertAlbedo;
     TextureAsset aTexDesertNormal;
     TextureAsset aTexDesertRoughness;
     TextureAsset aEnvmap;
-    
+
     Camera camera;
     FreeviewComponent freeview;
-    
+
     Light sun;
     Entity eSky;
     Cubemap envCubemap;
-    
+
     Entity eTerrain;
     Entity eCerberus;
-    
+
     FreeTypeFont font;
     Entity text;
     TextLine infoText;
-    
+
     NuklearGUI gui;
     bool envColorPicker = false;
     Color4f envColor = Color4f(0.8f, 0.8f, 1.0f, 1.0f);
-    
+
     float sunPitch = -45.0f;
     float sunTurn = 0.0f;
-    
+
     int useTextures = 1;
-    
+
     Color4f diffuseColor;
     bool diffuseColorPicker = false;
-    
+
     float roughness = 0.5f;
     float metallic = 0.0f;
-    
+
     this(Game game)
     {
         super(game);
         this.game = game;
         loadingScreen = New!LoadingScreen(game, this);
     }
-    
+
     string envmapPath = "data/TropicalRuins_Env.hdr";
-    
+
     override void beforeLoad()
     {
         aFont = addFontAsset("data/font/DroidSans.ttf", 14);
-        
+
         aMeshCerberus = addOBJAsset("data/cerberus/cerberus.obj");
         aTexCerberusAlbedo = addTextureAsset("data/cerberus/cerberus-albedo.png");
         aTexCerberusNormal = addTextureAsset("data/cerberus/cerberus-normal.png");
         aTexCerberusRoughness = addTextureAsset("data/cerberus/cerberus-roughness.png");
         aTexCerberusMetallic = addTextureAsset("data/cerberus/cerberus-metallic.png");
-        
+
         aHeightmap = addImageAsset("data/terrain/heightmap.png");
         aTexDesertAlbedo = addTextureAsset("data/terrain/desert-albedo.png");
         aTexDesertNormal = addTextureAsset("data/terrain/desert-normal.png");
         aTexDesertRoughness = addTextureAsset("data/terrain/desert-roughness.png");
-        
+
         aEnvmap = addTextureAsset(envmapPath);
     }
 
@@ -137,63 +137,63 @@ class Editor: Scene
         loadingScreen.update(t, progress);
         loadingScreen.render();
     }
-    
+
     override void afterLoad()
     {
         envCubemap = New!Cubemap(1024, assetManager);
         envCubemap.fromEquirectangularMap(aEnvmap.texture);
         environment.ambientMap = envCubemap;
-    
+
         camera = addCamera();
         freeview = New!FreeviewComponent(eventManager, camera);
         freeview.zoom(-100);
         game.renderer.activeCamera = camera;
-        
+
         game.deferredRenderer.ssaoPower = 6.0;
-        
+
         sun = addLight(LightType.Sun);
         sun.position.y = 50.0f;
         sun.shadowEnabled = true;
         sun.energy = 10.0f;
-        
+
         eSky = addEntity();
         eSky.layer = EntityLayer.Background;
         auto attach = New!AttachToCamera(eventManager, eSky, camera);
         eSky.drawable = New!ShapeBox(Vector3f(1.0f, 1.0f, 1.0f), assetManager);
         eSky.scaling = Vector3f(100.0f, 100.0f, 100.0f);
-        eSky.material = New!Material(null, assetManager);
+        eSky.material = New!Material(assetManager);
         eSky.material.depthWrite = false;
         eSky.material.culling = false;
         eSky.material.diffuse = envCubemap;
-        
+
         eTerrain = addEntity();
         eTerrain.position = Vector3f(-64, 0, -64);
-        eTerrain.material = New!Material(null, assetManager);
+        eTerrain.material = New!Material(assetManager);
         eTerrain.material.textureScale = Vector2f(10.0f, 10.0f);
         eTerrain.material.diffuse = aTexDesertAlbedo.texture;
         eTerrain.material.normal = aTexDesertNormal.texture;
         eTerrain.material.roughness = aTexDesertRoughness.texture;
-        
+
         auto heightmap = New!ImageHeightmap(aHeightmap.image, 30.0f, assetManager);
         auto terrain = New!Terrain(512, 64, heightmap, assetManager);
         eTerrain.drawable = terrain;
         eTerrain.scaling = Vector3f(0.25f, 0.25f, 0.25f);
-        
+
         eCerberus = addEntity();
         eCerberus.position.y = 7.0f;
         eCerberus.drawable = aMeshCerberus.mesh;
-        eCerberus.material = New!Material(null, assetManager);
+        eCerberus.material = New!Material(assetManager);
         eCerberus.material.diffuse = aTexCerberusAlbedo.texture;
         eCerberus.material.normal = aTexCerberusNormal.texture;
         eCerberus.material.roughness = aTexCerberusRoughness.texture;
         eCerberus.material.metallic = aTexCerberusMetallic.texture;
         diffuseColor = Color4f(0.5f, 0.5f, 0.5f, 1.0f);
-        
+
         gui = New!NuklearGUI(eventManager, assetManager);
         gui.addFont(aFont, 18, gui.localeGlyphRanges);
         auto eNuklear = addEntityHUD();
         eNuklear.drawable = gui;
-        
+
         text = addEntityHUD();
         infoText = New!TextLine(aFont.font, "Hello, World!", assetManager);
         infoText.color = Color4f(0.6f, 0.6f, 0.6f, 1.0f);
@@ -201,27 +201,27 @@ class Editor: Scene
         text.position.x = 10;
         text.position.y = eventManager.windowHeight - 10;
     }
-    
+
     char[100] textBuffer;
-    
+
     override void onUpdate(Time t)
     {
         text.position.y = eventManager.windowHeight - 10;
-        
+
         uint n = sprintf(textBuffer.ptr, "FPS: %u", eventManager.fps);
         string s = cast(string)textBuffer[0..n];
         infoText.setText(s);
-        
+
         updateUserInterface(t);
-        
+
         environment.backgroundColor = envColor;
         environment.ambientColor = envColor * 0.25f;
         environment.fogColor = envColor;
-        
-        sun.rotation = 
+
+        sun.rotation =
             rotationQuaternion!float(Axis.y, degtorad(sunTurn)) *
             rotationQuaternion!float(Axis.x, degtorad(sunPitch));
-        
+
         if (!useTextures)
         {
             eCerberus.material.diffuse = diffuseColor;
@@ -249,13 +249,13 @@ class Editor: Scene
         else if (key == KEY_A && eventManager.keyPressed[KEY_LCTRL])
             gui.inputKeyDown(NK_KEY_TEXT_SELECT_ALL);
     }
-    
+
     override void onKeyUp(int key)
     {
         if (key == KEY_BACKSPACE)
             gui.inputKeyUp(NK_KEY_BACKSPACE);
     }
-    
+
     override void onMouseButtonDown(int button)
     {
         gui.inputButtonDown(button);
@@ -263,7 +263,7 @@ class Editor: Scene
         freeview.prevMouseX = eventManager.mouseX;
         freeview.prevMouseY = eventManager.mouseY;
     }
-    
+
     override void onMouseButtonUp(int button)
     {
         gui.inputButtonUp(button);
@@ -280,20 +280,20 @@ class Editor: Scene
         if (!freeview.active)
             gui.inputScroll(x, y);
     }
-    
+
     override void onDropFile(string filename)
     {
         writeln(filename);
         assetManager.removeAsset(envmapPath);
         envmapPath = filename;
         aEnvmap = addTextureAsset(envmapPath, true);
-        
+
         if (envCubemap)
             deleteOwnedObject(envCubemap);
 
         envCubemap = New!Cubemap(1024, assetManager);
         envCubemap.fromEquirectangularMap(aEnvmap.texture);
-        
+
         environment.ambientMap = envCubemap;
         eSky.material.diffuse = envCubemap;
     }
@@ -301,7 +301,7 @@ class Editor: Scene
     void updateUserInterface(Time t)
     {
         gui.update(t);
-        
+
         if (gui.begin("Menu", NKRect(0, 0, eventManager.windowWidth, 40), 0))
         {
             gui.menubarBegin();
@@ -317,7 +317,7 @@ class Editor: Scene
                     if (gui.menuItemLabel("Exit", NK_TEXT_LEFT)) { application.exit(); }
                     gui.menuEnd();
                 }
-                
+
                 if (gui.menuBeginLabel("Edit", NK_TEXT_LEFT, NKVec2(200, 200)))
                 {
                     gui.layoutRowDynamic(25, 1);
@@ -325,7 +325,7 @@ class Editor: Scene
                     if (gui.menuItemLabel("Paste", NK_TEXT_LEFT)) { }
                     gui.menuEnd();
                 }
-                
+
                 if (gui.menuBeginLabel("Help", NK_TEXT_LEFT, NKVec2(200, 200)))
                 {
                     gui.layoutRowDynamic(25, 1);
@@ -336,51 +336,51 @@ class Editor: Scene
             gui.menubarEnd();
         }
         gui.end();
-        
+
         if (gui.begin("Properties", NKRect(0, 40, 300, eventManager.windowHeight - 40), NK_WINDOW_TITLE))
         {
             if (gui.treePush(NK_TREE_NODE, "Render", NK_MAXIMIZED))
             {
                 gui.layoutRowDynamic(30, 2);
                 gui.label("Output:", NK_TEXT_LEFT);
-                game.deferredRenderer.outputMode = 
+                game.deferredRenderer.outputMode =
                     cast(DebugOutputMode)gui.comboString(
-                        "Radiance\0Albedo\0Normal\0Position\0Roughness\0Metallic\0Occlusion", 
+                        "Radiance\0Albedo\0Normal\0Position\0Roughness\0Metallic\0Occlusion",
                         game.deferredRenderer.outputMode, 7, 25, NKVec2(120, 250));
-                
+
 				gui.layoutRowDynamic(25, 1);
 				game.deferredRenderer.ssaoSamples = gui.property("AO samples:", 1, game.deferredRenderer.ssaoSamples, 25, 1, 1);
 				game.deferredRenderer.ssaoRadius = gui.property("AO radius:", 0.05f, game.deferredRenderer.ssaoRadius, 1.0f, 0.01f, 0.005f);
                 game.deferredRenderer.ssaoPower = gui.property("AO power:", 0.0f, game.deferredRenderer.ssaoPower, 10.0f, 0.01f, 0.01f);
                 game.deferredRenderer.ssaoDenoise = gui.property("AO denoise:", 0.0f, game.deferredRenderer.ssaoDenoise, 1.0f, 0.01f, 0.01f);
-				
+
                 gui.layoutRowDynamic(25, 1);
                 game.postProcRenderer.glowThreshold = gui.property("Glow threshold:", 0.0f, game.postProcRenderer.glowThreshold, 1.0f, 0.01f, 0.005f);
                 game.postProcRenderer.glowIntensity = gui.property("Glow intensity:", 0.0f, game.postProcRenderer.glowIntensity, 1.0f, 0.01f, 0.005f);
-                
+
                 gui.layoutRowDynamic(25, 1);
                 game.postProcRenderer.glowRadius = gui.property("Glow radius:", 1, game.postProcRenderer.glowRadius, 10, 1, 1);
-                
+
                 gui.layoutRowDynamic(30, 2);
                 gui.label("Tonemapper:", NK_TEXT_LEFT);
-                game.postProcRenderer.tonemapper = 
+                game.postProcRenderer.tonemapper =
                     cast(Tonemapper)gui.comboString(
-                        "None\0Reinhard\0Hable\0ACES", 
+                        "None\0Reinhard\0Hable\0ACES",
                         game.postProcRenderer.tonemapper, 4, 25, NKVec2(120, 200));
-                
+
                 gui.layoutRowDynamic(25, 1);
                 game.postProcRenderer.exposure = gui.property("Exposure:", 0.0f, game.postProcRenderer.exposure, 2.0f, 0.01f, 0.005f);
-                
+
                 gui.treePop();
             }
-            
+
             if (gui.treePush(NK_TREE_NODE, "Environment", NK_MINIMIZED))
             {
                 gui.layoutRowDynamic(25, 2);
                 gui.label("Background color:", NK_TEXT_LEFT);
-                if (gui.buttonColor(envColor)) 
+                if (gui.buttonColor(envColor))
                     envColorPicker = !envColorPicker;
-                
+
                 if (envColorPicker)
                 {
                     NKRect s = NKRect(300, 100, 300, 350);
@@ -393,59 +393,59 @@ class Editor: Scene
                         envColor.g = gui.property("#G:", 0.0f, envColor.g, 1.0f, 0.01f, 0.005f);
                         envColor.b = gui.property("#B:", 0.0f, envColor.b, 1.0f, 0.01f, 0.005f);
                         gui.popupEnd();
-                    } 
+                    }
                     else envColorPicker = false;
                 }
-                
+
                 gui.layoutRowDynamic(25, 2);
                 gui.label("Sun pitch:", NK_TEXT_LEFT);
                 gui.slider(-180.0f, &sunPitch, 180.0f, 1.0f);
-                
+
                 gui.layoutRowDynamic(25, 2);
                 gui.label("Sun turn:", NK_TEXT_LEFT);
                 gui.slider(-180.0f, &sunTurn, 180.0f, 1.0f);
-                
+
                 gui.layoutRowDynamic(25, 2);
                 gui.label("Sun energy:", NK_TEXT_LEFT);
                 gui.slider(0.0f, &sun.energy, 50.0f, 1.0f);
-                
+
                 gui.treePop();
             }
-            
+
             if (gui.treePush(NK_TREE_NODE, "Material", NK_MAXIMIZED))
             {
                 gui.layoutRowDynamic(25, 2);
                 gui.checkboxLabel("Textures", &useTextures);
-                
+
                 gui.layoutRowDynamic(25, 2);
                 gui.label("Diffuse color:", NK_TEXT_LEFT);
-                if (gui.buttonColor(diffuseColor)) 
+                if (gui.buttonColor(diffuseColor))
                     diffuseColorPicker = !diffuseColorPicker;
-                
+
                 if (diffuseColorPicker)
                 {
                     NKRect s = NKRect(300, 100, 300, 350);
                     if (gui.popupBegin(NK_POPUP_STATIC, "Color", NK_WINDOW_CLOSABLE, s))
                     {
-                        gui.layoutRowDynamic(180, 1); 
+                        gui.layoutRowDynamic(180, 1);
                         diffuseColor = gui.colorPicker(diffuseColor, NK_RGB);
                         gui.layoutRowDynamic(25, 1);
                         diffuseColor.r = gui.property("#R:", 0.0f, diffuseColor.r, 1.0f, 0.01f, 0.005f);
                         diffuseColor.g = gui.property("#G:", 0.0f, diffuseColor.g, 1.0f, 0.01f, 0.005f);
                         diffuseColor.b = gui.property("#B:", 0.0f, diffuseColor.b, 1.0f, 0.01f, 0.005f);
                         gui.popupEnd();
-                    } 
+                    }
                     else diffuseColorPicker = false;
                 }
-                
+
                 gui.layoutRowDynamic(25, 1);
                 roughness = gui.property("Roughness:", 0.0f, roughness, 1.0f, 0.01f, 0.005f);
-                
+
                 gui.layoutRowDynamic(25, 1);
                 metallic = gui.property("Metallic:", 0.0f, metallic, 1.0f, 0.01f, 0.005f);
                 gui.treePop();
             }
-            
+
             /*
             if (gui.treePush(NK_TREE_NODE, "Options", NK_MINIMIZED))
             {
@@ -454,7 +454,7 @@ class Editor: Scene
                 if (gui.optionLabel("off", option == false)) option = false;
                 gui.treePop();
             }
-                
+
             if (gui.treePush(NK_TREE_NODE, "Input", NK_MINIMIZED))
             {
                 static int len = 4;
