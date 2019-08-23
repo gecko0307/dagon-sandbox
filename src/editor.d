c103f51231d3c0a4bc690564267279e395c08650
TimurGafarov <gt@smarthead.ru>
2019-08-23 13:09:57 +0300

    Broke up updateUserInterface to multiple functions

--- src/editor.d
+++ src/editor.d
@@ -36,7 +36,7 @@ class Editor: Scene
     Game game;
 
     LoadingScreen loadingScreen;
-    
+
 
     FontAsset aFont;
 
@@ -297,6 +297,8 @@ class Editor: Scene
             application.exit();
         else if (key == KEY_BACKSPACE)
             gui.inputKeyDown(NK_KEY_BACKSPACE);
+        else if (key == KEY_DELETE)
+            gui.inputKeyDown(NK_KEY_DEL);
         else if (key == KEY_C && eventManager.keyPressed[KEY_LCTRL])
             gui.inputKeyDown(NK_KEY_COPY);
         else if (key == KEY_V && eventManager.keyPressed[KEY_LCTRL])
@@ -351,10 +353,8 @@ class Editor: Scene
         eSky.material.diffuse = envCubemap;
     }
 
-    void updateUserInterface(Time t)
+    void updateMenu()
     {
-        gui.update(t);
-
         if (gui.begin("Menu", NKRect(0, 0, eventManager.windowWidth, 40), 0))
         {
             gui.menubarBegin();
@@ -389,151 +389,170 @@ class Editor: Scene
             gui.menubarEnd();
         }
         gui.end();
+    }
 
+    void updateRenderTab()
+    {
+        if (gui.treePush(NK_TREE_NODE, "Render", NK_MAXIMIZED))
+        {
+            gui.layoutRowDynamic(30, 2);
+            gui.label("Output:", NK_TEXT_LEFT);
+            auto oldOutputMode = game.deferredRenderer.outputMode;
+            game.deferredRenderer.outputMode =
+                cast(DebugOutputMode)gui.comboString(
+                    "Radiance\0Albedo\0Normal\0Position\0Roughness\0Metallic\0Occlusion",
+                    game.deferredRenderer.outputMode, 7, 25, NKVec2(120, 250));
+            if (game.deferredRenderer.outputMode != oldOutputMode)
+                writeln("Output mode: ", game.deferredRenderer.outputMode);
+
+            gui.layoutRowDynamic(25, 1);
+            game.deferredRenderer.ssaoSamples = gui.property("AO samples:", 1, game.deferredRenderer.ssaoSamples, 25, 1, 1);
+            game.deferredRenderer.ssaoRadius = gui.property("AO radius:", 0.05f, game.deferredRenderer.ssaoRadius, 1.0f, 0.01f, 0.005f);
+            game.deferredRenderer.ssaoPower = gui.property("AO power:", 0.0f, game.deferredRenderer.ssaoPower, 10.0f, 0.01f, 0.01f);
+            game.deferredRenderer.ssaoDenoise = gui.property("AO denoise:", 0.0f, game.deferredRenderer.ssaoDenoise, 1.0f, 0.01f, 0.01f);
+
+            gui.layoutRowDynamic(25, 1);
+            game.postProcessingRenderer.glowThreshold = gui.property("Glow threshold:", 0.0f, game.postProcessingRenderer.glowThreshold, 1.0f, 0.01f, 0.005f);
+            game.postProcessingRenderer.glowIntensity = gui.property("Glow intensity:", 0.0f, game.postProcessingRenderer.glowIntensity, 1.0f, 0.01f, 0.005f);
+
+            gui.layoutRowDynamic(25, 1);
+            game.postProcessingRenderer.glowRadius = gui.property("Glow radius:", 1, game.postProcessingRenderer.glowRadius, 10, 1, 1);
+
+            gui.layoutRowDynamic(30, 2);
+            gui.label("Tonemapper:", NK_TEXT_LEFT);
+            game.postProcessingRenderer.tonemapper =
+                cast(Tonemapper)gui.comboString(
+                    "None\0Reinhard\0Hable\0ACES",
+                    game.postProcessingRenderer.tonemapper, 4, 25, NKVec2(120, 200));
+
+            gui.layoutRowDynamic(25, 1);
+            game.postProcessingRenderer.exposure = gui.property("Exposure:", 0.0f, game.postProcessingRenderer.exposure, 2.0f, 0.01f, 0.005f);
+
+            gui.treePop();
+        }
+    }
+
+    void updateEnvironmentTab()
+    {
+        if (gui.treePush(NK_TREE_NODE, "Environment", NK_MINIMIZED))
+        {
+            gui.layoutRowDynamic(25, 2);
+            gui.label("Background color:", NK_TEXT_LEFT);
+            if (gui.buttonColor(envColor))
+                envColorPicker = !envColorPicker;
+            if (envColorPicker)
+            {
+                NKRect s = NKRect(300, 100, 300, 350);
+                if (gui.popupBegin(NK_POPUP_STATIC, "Color", NK_WINDOW_CLOSABLE, s))
+                {
+                    gui.layoutRowDynamic(180, 1);
+                    envColor = gui.colorPicker(envColor, NK_RGB);
+                    gui.layoutRowDynamic(25, 1);
+                    envColor.r = gui.property("#R:", 0.0f, envColor.r, 1.0f, 0.01f, 0.005f);
+                    envColor.g = gui.property("#G:", 0.0f, envColor.g, 1.0f, 0.01f, 0.005f);
+                    envColor.b = gui.property("#B:", 0.0f, envColor.b, 1.0f, 0.01f, 0.005f);
+                    gui.popupEnd();
+                }
+                else envColorPicker = false;
+            }
+
+            gui.layoutRowDynamic(25, 2);
+            gui.label("Sun color:", NK_TEXT_LEFT);
+            if (gui.buttonColor(sunColor))
+                sunColorPicker = !sunColorPicker;
+            if (sunColorPicker)
+            {
+                NKRect s = NKRect(300, 100, 300, 350);
+                if (gui.popupBegin(NK_POPUP_STATIC, "Color", NK_WINDOW_CLOSABLE, s))
+                {
+                    gui.layoutRowDynamic(180, 1);
+                    sunColor = gui.colorPicker(sunColor, NK_RGB);
+                    gui.layoutRowDynamic(25, 1);
+                    sunColor.r = gui.property("#R:", 0.0f, sunColor.r, 1.0f, 0.01f, 0.005f);
+                    sunColor.g = gui.property("#G:", 0.0f, sunColor.g, 1.0f, 0.01f, 0.005f);
+                    sunColor.b = gui.property("#B:", 0.0f, sunColor.b, 1.0f, 0.01f, 0.005f);
+                    gui.popupEnd();
+                }
+                else sunColorPicker = false;
+            }
+
+            gui.layoutRowDynamic(25, 1);
+            gui.label("Rayleigh sky:", NK_TEXT_LEFT);
+            gui.layoutRowDynamic(25, 2);
+            if (gui.optionLabel("on", useSky == true)) useSky = true;
+            if (gui.optionLabel("off", useSky == false)) useSky = false;
+
+            gui.layoutRowDynamic(25, 2);
+            gui.label("Sun pitch:", NK_TEXT_LEFT);
+            gui.slider(-180.0f, &sunPitch, 180.0f, 0.01f);
+
+            gui.layoutRowDynamic(25, 2);
+            gui.label("Sun turn:", NK_TEXT_LEFT);
+            gui.slider(-180.0f, &sunTurn, 180.0f, 0.01f);
+
+            gui.layoutRowDynamic(25, 2);
+            gui.label("Sun energy:", NK_TEXT_LEFT);
+            gui.slider(0.0f, &sun.energy, 50.0f, 0.01f);
+
+            gui.layoutRowDynamic(25, 2);
+            int sc = sun.scatteringEnabled;
+            gui.checkboxLabel("Volumetric light", &sc);
+            sun.scatteringEnabled = cast(bool)sc;
+            gui.layoutRowDynamic(25, 2);
+            gui.label("Scattering:", NK_TEXT_LEFT);
+            gui.slider(0.0f, &sun.scattering, 1.0f, 0.01f);
+            gui.layoutRowDynamic(25, 2);
+            gui.label("Medium density:", NK_TEXT_LEFT);
+            gui.slider(0.0f, &sun.mediumDensity, 1.0f, 0.01f);
+
+            gui.treePop();
+        }
+    }
+
+    void updateMaterialTab()
+    {
+        if (gui.treePush(NK_TREE_NODE, "Material", NK_MAXIMIZED))
+        {
+            gui.layoutRowDynamic(25, 2);
+            gui.checkboxLabel("Textures", &useTextures);
+
+            gui.layoutRowDynamic(25, 2);
+            gui.label("Diffuse color:", NK_TEXT_LEFT);
+            if (gui.buttonColor(diffuseColor))
+                diffuseColorPicker = !diffuseColorPicker;
+
+            if (diffuseColorPicker)
+            {
+                NKRect s = NKRect(300, 100, 300, 350);
+                if (gui.popupBegin(NK_POPUP_STATIC, "Color", NK_WINDOW_CLOSABLE, s))
+                {
+                    gui.layoutRowDynamic(180, 1);
+                    diffuseColor = gui.colorPicker(diffuseColor, NK_RGB);
+                    gui.layoutRowDynamic(25, 1);
+                    diffuseColor.r = gui.property("#R:", 0.0f, diffuseColor.r, 1.0f, 0.01f, 0.005f);
+                    diffuseColor.g = gui.property("#G:", 0.0f, diffuseColor.g, 1.0f, 0.01f, 0.005f);
+                    diffuseColor.b = gui.property("#B:", 0.0f, diffuseColor.b, 1.0f, 0.01f, 0.005f);
+                    gui.popupEnd();
+                }
+                else diffuseColorPicker = false;
+            }
+
+            gui.layoutRowDynamic(25, 1);
+            roughness = gui.property("Roughness:", 0.0f, roughness, 1.0f, 0.01f, 0.005f);
+
+            gui.layoutRowDynamic(25, 1);
+            metallic = gui.property("Metallic:", 0.0f, metallic, 1.0f, 0.01f, 0.005f);
+            gui.treePop();
+        }
+    }
+
+    void updatePropertiesPanel()
+    {
         if (gui.begin("Properties", NKRect(0, 40, 300, eventManager.windowHeight - 40), NK_WINDOW_TITLE))
         {
-            if (gui.treePush(NK_TREE_NODE, "Render", NK_MAXIMIZED))
-            {
-                gui.layoutRowDynamic(30, 2);
-                gui.label("Output:", NK_TEXT_LEFT);
-                game.deferredRenderer.outputMode =
-                    cast(DebugOutputMode)gui.comboString(
-                        "Radiance\0Albedo\0Normal\0Position\0Roughness\0Metallic\0Occlusion",
-                        game.deferredRenderer.outputMode, 7, 25, NKVec2(120, 250));
-
-                gui.layoutRowDynamic(25, 1);
-                game.deferredRenderer.ssaoSamples = gui.property("AO samples:", 1, game.deferredRenderer.ssaoSamples, 25, 1, 1);
-                game.deferredRenderer.ssaoRadius = gui.property("AO radius:", 0.05f, game.deferredRenderer.ssaoRadius, 1.0f, 0.01f, 0.005f);
-                game.deferredRenderer.ssaoPower = gui.property("AO power:", 0.0f, game.deferredRenderer.ssaoPower, 10.0f, 0.01f, 0.01f);
-                game.deferredRenderer.ssaoDenoise = gui.property("AO denoise:", 0.0f, game.deferredRenderer.ssaoDenoise, 1.0f, 0.01f, 0.01f);
-
-                gui.layoutRowDynamic(25, 1);
-                game.postProcessingRenderer.glowThreshold = gui.property("Glow threshold:", 0.0f, game.postProcessingRenderer.glowThreshold, 1.0f, 0.01f, 0.005f);
-                game.postProcessingRenderer.glowIntensity = gui.property("Glow intensity:", 0.0f, game.postProcessingRenderer.glowIntensity, 1.0f, 0.01f, 0.005f);
-
-                gui.layoutRowDynamic(25, 1);
-                game.postProcessingRenderer.glowRadius = gui.property("Glow radius:", 1, game.postProcessingRenderer.glowRadius, 10, 1, 1);
-
-                gui.layoutRowDynamic(30, 2);
-                gui.label("Tonemapper:", NK_TEXT_LEFT);
-                game.postProcessingRenderer.tonemapper =
-                    cast(Tonemapper)gui.comboString(
-                        "None\0Reinhard\0Hable\0ACES",
-                        game.postProcessingRenderer.tonemapper, 4, 25, NKVec2(120, 200));
-
-                gui.layoutRowDynamic(25, 1);
-                game.postProcessingRenderer.exposure = gui.property("Exposure:", 0.0f, game.postProcessingRenderer.exposure, 2.0f, 0.01f, 0.005f);
-
-                gui.treePop();
-            }
-
-            if (gui.treePush(NK_TREE_NODE, "Environment", NK_MINIMIZED))
-            {
-                gui.layoutRowDynamic(25, 2);
-                gui.label("Background color:", NK_TEXT_LEFT);
-                if (gui.buttonColor(envColor))
-                    envColorPicker = !envColorPicker;
-                if (envColorPicker)
-                {
-                    NKRect s = NKRect(300, 100, 300, 350);
-                    if (gui.popupBegin(NK_POPUP_STATIC, "Color", NK_WINDOW_CLOSABLE, s))
-                    {
-                        gui.layoutRowDynamic(180, 1);
-                        envColor = gui.colorPicker(envColor, NK_RGB);
-                        gui.layoutRowDynamic(25, 1);
-                        envColor.r = gui.property("#R:", 0.0f, envColor.r, 1.0f, 0.01f, 0.005f);
-                        envColor.g = gui.property("#G:", 0.0f, envColor.g, 1.0f, 0.01f, 0.005f);
-                        envColor.b = gui.property("#B:", 0.0f, envColor.b, 1.0f, 0.01f, 0.005f);
-                        gui.popupEnd();
-                    }
-                    else envColorPicker = false;
-                }
-
-                gui.layoutRowDynamic(25, 2);
-                gui.label("Sun color:", NK_TEXT_LEFT);
-                if (gui.buttonColor(sunColor))
-                    sunColorPicker = !sunColorPicker;
-                if (sunColorPicker)
-                {
-                    NKRect s = NKRect(300, 100, 300, 350);
-                    if (gui.popupBegin(NK_POPUP_STATIC, "Color", NK_WINDOW_CLOSABLE, s))
-                    {
-                        gui.layoutRowDynamic(180, 1);
-                        sunColor = gui.colorPicker(sunColor, NK_RGB);
-                        gui.layoutRowDynamic(25, 1);
-                        sunColor.r = gui.property("#R:", 0.0f, sunColor.r, 1.0f, 0.01f, 0.005f);
-                        sunColor.g = gui.property("#G:", 0.0f, sunColor.g, 1.0f, 0.01f, 0.005f);
-                        sunColor.b = gui.property("#B:", 0.0f, sunColor.b, 1.0f, 0.01f, 0.005f);
-                        gui.popupEnd();
-                    }
-                    else sunColorPicker = false;
-                }
-
-                gui.layoutRowDynamic(25, 1);
-                gui.label("Rayleigh sky:", NK_TEXT_LEFT);
-                gui.layoutRowDynamic(25, 2);
-                if (gui.optionLabel("on", useSky == true)) useSky = true;
-                if (gui.optionLabel("off", useSky == false)) useSky = false;
-
-                gui.layoutRowDynamic(25, 2);
-                gui.label("Sun pitch:", NK_TEXT_LEFT);
-                gui.slider(-180.0f, &sunPitch, 180.0f, 0.01f);
-
-                gui.layoutRowDynamic(25, 2);
-                gui.label("Sun turn:", NK_TEXT_LEFT);
-                gui.slider(-180.0f, &sunTurn, 180.0f, 0.01f);
-
-                gui.layoutRowDynamic(25, 2);
-                gui.label("Sun energy:", NK_TEXT_LEFT);
-                gui.slider(0.0f, &sun.energy, 50.0f, 0.01f);
-
-                gui.layoutRowDynamic(25, 2);
-                int sc = sun.scatteringEnabled;
-                gui.checkboxLabel("Volumetric light", &sc);
-                sun.scatteringEnabled = cast(bool)sc;
-                gui.layoutRowDynamic(25, 2);
-                gui.label("Scattering:", NK_TEXT_LEFT);
-                gui.slider(0.0f, &sun.scattering, 1.0f, 0.01f);
-                gui.layoutRowDynamic(25, 2);
-                gui.label("Medium density:", NK_TEXT_LEFT);
-                gui.slider(0.0f, &sun.mediumDensity, 1.0f, 0.01f);
-
-                gui.treePop();
-            }
-
-            if (gui.treePush(NK_TREE_NODE, "Material", NK_MAXIMIZED))
-            {
-                gui.layoutRowDynamic(25, 2);
-                gui.checkboxLabel("Textures", &useTextures);
-
-                gui.layoutRowDynamic(25, 2);
-                gui.label("Diffuse color:", NK_TEXT_LEFT);
-                if (gui.buttonColor(diffuseColor))
-                    diffuseColorPicker = !diffuseColorPicker;
-
-                if (diffuseColorPicker)
-                {
-                    NKRect s = NKRect(300, 100, 300, 350);
-                    if (gui.popupBegin(NK_POPUP_STATIC, "Color", NK_WINDOW_CLOSABLE, s))
-                    {
-                        gui.layoutRowDynamic(180, 1);
-                        diffuseColor = gui.colorPicker(diffuseColor, NK_RGB);
-                        gui.layoutRowDynamic(25, 1);
-                        diffuseColor.r = gui.property("#R:", 0.0f, diffuseColor.r, 1.0f, 0.01f, 0.005f);
-                        diffuseColor.g = gui.property("#G:", 0.0f, diffuseColor.g, 1.0f, 0.01f, 0.005f);
-                        diffuseColor.b = gui.property("#B:", 0.0f, diffuseColor.b, 1.0f, 0.01f, 0.005f);
-                        gui.popupEnd();
-                    }
-                    else diffuseColorPicker = false;
-                }
-
-                gui.layoutRowDynamic(25, 1);
-                roughness = gui.property("Roughness:", 0.0f, roughness, 1.0f, 0.01f, 0.005f);
-
-                gui.layoutRowDynamic(25, 1);
-                metallic = gui.property("Metallic:", 0.0f, metallic, 1.0f, 0.01f, 0.005f);
-                gui.treePop();
-            }
+            updateRenderTab();
+            updateEnvironmentTab();
+            updateMaterialTab();
 
             /*
             if (gui.treePush(NK_TREE_NODE, "Options", NK_MINIMIZED))
@@ -555,5 +574,12 @@ class Editor: Scene
             */
         }
         gui.end();
+    }
+
+    void updateUserInterface(Time t)
+    {
+        gui.update(t);
+        updateMenu();
+        updatePropertiesPanel();
     }
 }
